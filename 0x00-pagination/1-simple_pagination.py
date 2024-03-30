f8d12addb02b3942df2f5310be711a2d71876561
Rophpad <rophenp@gmail.com>
2024-03-30 16:21:12 +0100

    Simple pagination on a dataset

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+"""
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Returns a tuple containing  start index and an end index
+    """
+    end_idx = page * page_size
+    return (end_idx - page_size, end_idx)
+
+
+class Server:
+    """
+    Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """
+        Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Paginates datasetand return appropriate page
+        """
+        assert isinstance(page, int) and page > 0\
+            and isinstance(page_size, int) and page_size > 0
+        start, end = index_range(page, page_size)
+        dataset = self.dataset()
+        return [] if (start >= len(dataset) or end >= len(dataset))\
+            else dataset[start:end]
