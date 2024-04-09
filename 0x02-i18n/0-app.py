7277a2ed1bb8c867083981232d9d15f609620baa
Rophpad <rophenp@gmail.com>
2024-04-09 13:18:58 +0100

    Basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Setup Flask app"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+port = 5000
+
+
+@app.route('/')
+@app.route('/index', strict_slashes=False)
+def index():
+    """Render index.html"""
+
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=port)
