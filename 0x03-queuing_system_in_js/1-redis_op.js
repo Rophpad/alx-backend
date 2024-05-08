22283f07fb2c2481cb6b90234351a4f2e567d9bd
Rophpad <rophenp@gmail.com>
2024-05-08 14:48:15 +0100

    some basic ops with node Redis client

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,26 @@
+import redis from 'redis';
+const client = redis.createClient();
+
+client
+  .on("error", (error) => {
+    console.log(`Redis client not connected to the server: ${error.message}`);
+  })
+  .on("connect", () => {
+    console.log("Redis client connected to the server");
+  });
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, (err, reply) => {
+    redis.print(`Redis: ${reply}`);
+  });
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, reply) => {
+    console.log(reply);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
